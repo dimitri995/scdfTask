43c50a67726ed3eff5f601b446c8ce0797908923
Dimitri.HEBRARD <dimitri.hebrard@berger-levrault.com>
2022-11-09 10:41:27 +0100

    ok

---
+++ src/main/java/com/example/springtask/TraceTest.java
@@ -0,0 +1,60 @@
+package com.example.springtask;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.*;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TraceTest implements ApplicationRunner {
+
+    @Autowired
+    OpenTelemetry openTelemetry;
+
+    private final Log logger = LogFactory.getLog(TraceTest.class);
+
+
+    @Override
+    public void run(ApplicationArguments arg0) throws InterruptedException {
+//            List<String> traceIdList = Collections.singletonList(arg0.getOptionValues("traceId").toString());
+//            String traceId = traceIdList.get(0);
+//            List<String> spanIdList = Collections.singletonList(arg0.getOptionValues("spanId").toString());
+//            String spanId = spanIdList.get(0);
+//            List<String> traceStateList = Collections.singletonList(arg0.getOptionValues("traceState").toString());
+//            String traceState = traceStateList.get(0);
+//            List<String> traceFlagList = Collections.singletonList(arg0.getOptionValues("traceFlag").toString());
+//            String traceFlag = traceFlagList.get(0);
+
+            SpanContext remoteContext = SpanContext.createFromRemoteParent(
+                    "0f161df96490f243b9e2d38105b0c0e6",
+                    "fc9caa2db2bedec1",
+                    TraceFlags.getSampled(),
+                    TraceState.getDefault());
+
+////            Tracer tracer = openTelemetry.getTracer("ok");
+        Context.root().with(Span.wrap(remoteContext));
+
+            Span span = openTelemetry.getTracer("d").spanBuilder("spanbuilder")
+                    .setParent(Context.current().with(Span.wrap(remoteContext))).startSpan();
+//            Span span = openTelemetry.getTracer("tttttt").spanBuilder("spanbuilder").startSpan();
+
+
+//        Tracer tracer = openTelemetry.getTracer("spring-cloud");
+//
+//        Span span = tracer.spanBuilder("span").startSpan();
+//        Context.current().makeCurrent();
+        Scope ss = span.makeCurrent();
+        logger.info(Span.current().getSpanContext().getTraceId());
+        logger.info(Span.current().getSpanContext().getSpanId());
+        logger.info("Test trace");
+        span.end();
+
+    }
+}
---
+++ src/main/java/com/example/springtask/configuration/OtelConfig.java
@@ -0,0 +1,50 @@
+package com.example.springtask.configuration;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OtelConfig {
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "taskService")));
+
+        SpanExporter spanExporter = OtlpHttpSpanExporter.builder()
+                .setEndpoint("http://localhost:4318/v1/traces") //TODO Replace <URL> to the Collector URL
+                .build();
+
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .setResource(resource)
+                .build();
+
+//        SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+//                .registerMetricReader(PeriodicMetricReader.builder(OtlpHttpMetricExporter.builder().build()).build())
+//                .setResource(resource)
+//                .build();
+
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+//                .setMeterProvider(sdkMeterProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .buildAndRegisterGlobal();
+
+        return openTelemetry;
+    }
+
+}
