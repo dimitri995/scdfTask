38bbd7d4b06aa3dd45d26e0d2c7c5fd1059a2519
Dimitri.HEBRARD <dimitri.hebrard@berger-levrault.com>
2022-11-10 14:11:31 +0100

    add kafka trace

--- src/main/java/com/example/springtask/configuration/KafkaProducerConfig.java
+++ src/main/java/com/example/springtask/configuration/KafkaProducerConfig.java
@@ -34,7 +34,8 @@ public class KafkaProducerConfig {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "broker:9092");
+                "broker:29092");
+//                "localhost:9092");
         configProps.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
